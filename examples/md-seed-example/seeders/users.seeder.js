7eaf93396db16bc2c235ab1051e2cad339600ba1
Avi Sharvit <sharvita@gmail.com>
2019-07-06 14:09:06 +0300

    refactor(example): adds custome-template to the example

--- examples/md-seed-example/seeders/users.seeder.js
+++ examples/md-seed-example/seeders/users.seeder.js
@@ -36,9 +36,9 @@ const data = [
 
 class UsersSeeder extends Seeder {
   async shouldRun() {
-    return User.countDocuments()
-      .exec()
-      .then(count => count === 0);
+    const count = await User.countDocuments().exec();
+
+    return count === 0;
   }
 
   async run() {
